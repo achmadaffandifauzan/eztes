db42884d9a576c42e6b44db3135015ca7315636a
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2023-03-31 01:20:21 +0700

    design and style update

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,15 @@ if (questionTypeOptions) {
         }
     });
 }
+// giving background of answer in evaluate page base on it's correctness (by detecting it's googleIcon's name)
+const symbolAnswer = document.querySelectorAll(".material-symbols-rounded");
+for (let symbol of symbolAnswer) {
+    if (symbol.innerText == "verified" || symbol.innerText == "key") {
+        symbol.parentElement.parentElement.classList.add("correctAnswer")
+    } else if (symbol.innerText == "block") {
+        symbol.parentElement.parentElement.classList.add("wrongAnswer")
+    }
+};
+
+// removing search in nav when page is index category
+const navSearch = document.querySelector("#navSearch");
